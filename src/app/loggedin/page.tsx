ecc8df0f0f3a793ce0102f6bc029957f789964d1
Aliasgar Sogiawala <vertexxinfotech18@gmail.com>
2025-08-23 21:26:34 +0530

    Refactor layout and authentication structure; remove ConvexClientProvider, add LoggedInLayout and Providers components

--- src/app/loggedin/page.tsx
+++ src/app/loggedin/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import Tasks from "@/components/planify/tasks";
+import UserProfile from "@/components/planify/user-profile";
 
 
 export default function Home() {
   return (
       <main className="flex flex-col items-center justify-between p-24">
         <h1>Tasknest</h1>
-
+        <UserProfile/>
         <Tasks />
       </main>
       
