b05c2978f154e95bf6df312e5ea4fc897492f815
Aliasgar Sogiawala <vertexxinfotech18@gmail.com>
2025-08-24 09:40:08 +0530

    feat: implement Task component and integrate it into TodoList for improved task rendering

--- src/components/todos/todo-list.tsx
+++ src/components/todos/todo-list.tsx
@@ -3,12 +3,13 @@
 import { useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 import { Checkbox } from "../ui/checkbox";
+import Task from "./task";
 
 export default function TodoList() {
     const todos= useQuery(api.todos.get) ?? [];
 
     if(todos === undefined){
-        <p>Loading..</p>
+        return <p>Loading..</p>
     }
   return <div className="xl:px-40" >
     <div className="text-lg font-semibold md:text-2xl">
@@ -18,18 +19,10 @@ export default function TodoList() {
     </div>
 
     <div className="flex flex-col gap-1 py-4">
-        <div className="flex items-center space-x-2">
-            {todos.map(({taskName},idx)=>(
-                <div key={idx}>
-                    <Checkbox id="todo"/>
-                    <label
-                    htmlFor="todo"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed
-                    peer-disabled:opacity-70"
-                    >{taskName}</label>
-                </div>
+            {todos.map((task, idx)=>(
+                <Task {...task} key={task._id} />
             ))}
-        </div>
+        
     </div>
   </div>
     
